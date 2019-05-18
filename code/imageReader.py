8741fd0f8abc2a16ce0e04f4eea573d15fde53ce
Jessy <liaojessy18@gmail.com>
2019-05-18 13:29:56 -0600

    Added skeleton code/template

---
+++ code/imageReader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#######################################################################################
+# Name:         ImageReader
+#
+# Last Updated: 5/18/19
+#
+#######################################################################################
+
+import sys
+
+class imageReader:
+
+
+    def __init__(self): 
+        self.layers = [] # array of layer objects
+        self.points = [] # array of point objects
+
+
+    # TODO
+    def readLayer(self): 
+        return 0 
+
+
+    # TODO
+    def getPoints(self):
+        return self.points
+
---
+++ code/layers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#######################################################################################
+# Name:         Layers 
+#
+# Last Updated: 5/18/19
+#
+#######################################################################################
+
+import sys
+
+class layers:
+
+
+    def __init__(self): 
+       self.points = [] # array of point objects (i think)
+       self.covered = False # determine if segment of outcrop is usable
+       # Implement the following if time permits: 
+       # - rockColor
+       # - rockName
+       # - rockType
+
+
+    def getPoints(self):
+        return self.points
+
+
+    # TODO
+    def setPoints(self):
+        return 0
+    
