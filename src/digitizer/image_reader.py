4fb638c60b48f817dbe0be8955d416ca09ff6a5f
Jessy <liaojessy18@gmail.com>
2019-06-09 13:53:45 -0600

    fixed tabs, 4 spaces is superior

--- src/digitizer/image_reader.py
+++ src/digitizer/image_reader.py
@@ -13,28 +13,28 @@ import imutils
 class ImageReader:
 
     def __init__(self, image): 
-		self.image = image
-		scale = self.image.shape[0]/650
-		self.image = cv2.resize(self.image, (int(self.image.shape[1]/scale), 650))
+        self.image = image
+        scale = self.image.shape[0]/650
+        self.image = cv2.resize(self.image, (int(self.image.shape[1]/scale), 650))
 
-	
-	@image.setter
-	def image(self, img):
-		self.image = img
+    
+    @image.setter
+    def image(self, img):
+        self.image = img
 
-	def get_image(self, img):
-		return self.image
-	
-	def prune(self):
-		thresh = cv2.Canny(img, 10, 55)
-		contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-		
-		for c in contours:
-        	if cv2.contourArea(c) >  200:
-            	approx = cv2.approxPolyDP(c, 0.0001*cv2.arcLength(c, True), True)
-                cv2.drawContours(img, [approx], -1, (0, 255, 0), 1)
+    def get_image(self, img):
+        return self.image
+    
+    def prune(self):
+        thresh = cv2.Canny(img, 10, 55)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        
+        for c in contours:
+        if cv2.contourArea(c) >  200:
+        approx = cv2.approxPolyDP(c, 0.0001*cv2.arcLength(c, True), True)
+        cv2.drawContours(img, [approx], -1, (0, 255, 0), 1)
 
-		return self.image
+        return self.image
 
 
 
--- src/digitizer/skeleton_creator.py
+++ src/digitizer/skeleton_creator.py
@@ -1,10 +1,10 @@
 
 class SkeletonCreator:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def create_layers(self):
-		pass
+    def create_layers(self):
+        pass
 
-	def create_csv(self):
-		pass
+    def create_csv(self):
+        pass
