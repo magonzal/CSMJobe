ca27ae1121d465d2779cf124aef00c26670f95bd
Marcelo Gonzales <magonzal@mymail.mines.edu>
2019-06-12 10:38:43 -0600

    Added one test case

--- tests/test_image_reader.py
+++ tests/test_image_reader.py
@@ -1,15 +1,26 @@
 import pytest
 from src.digitizer.image_reader import ImageReader
+import cv2
+import numpy as np
+
 
 @pytest.fixture()
 def image():
-    pass
+    image_to_test = cv2.imread('test3.jpg')
+    return image_to_test
 
-def test_set_image():
-    pass
 
-def test_get_image():
-    pass
+def test_get_image(image):
+    test_one = ImageReader(image)
+    case_one = cv2.imread('test3.jpg')
+
+    assert type(test_one.get_image()) == np.ndarray
+    assert test_one.get_image().shape == (650, 104, case_one.shape[2])
+
+
+def test_set_image(image):
+    test_one = ImageReader(image)
+
 
 def test_prune():
     pass
